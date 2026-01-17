477b06c701f84d725bf39882545a43b1cc4516fb
riccardo-5 <rtoso3690@gmail.com>
2026-01-17 10:01:47 +0100

    further implementation

---
+++ README.txt
@@ -0,0 +1,3 @@
+Riccardo Tosin si e' occupato dell'implementazione del simulatore
+Federico Battistella si e' occputao dell'implementazione del tutor
+Domenico Longo si e' occupato del sistema di creazione dell'autostrada
---
+++ src/GenerateSimulation.cpp
@@ -0,0 +1,9 @@
+#include "../include/Simulator.h"
+
+int main() 
+{
+    Highway my_highway("../Data/Highway1.txt");
+    Simulator my_simulator(my_highway);
+    my_simulator.generateRuns();
+    my_simulator.generatePassages();
+}
